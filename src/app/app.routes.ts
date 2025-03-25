3d0a163ce5631e2843fab2f654fa914609a1d968
Idir FOUQUET <fouquetidir@gmail.com>
2025-03-25 16:10:52 +0100

    Remove angular and next install

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from "@angular/router";
-import { DestinationsComponent } from "./pages/destinations/destinations.component";
-import { AppComponent } from "./app.component"; // Importer AppComponent
-
-export const routes: Routes = [
-  { path: '', component: AppComponent },  // AppComponent est la Homepage
-  { path: 'destinations', component: DestinationsComponent }  
-];
--- src/app/components/planets/planets.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-planets',
-  standalone: true,
-  templateUrl: './planets.component.html',
-  styleUrls: ['./planets.component.css']
-})
-export class PlanetsComponent {}
