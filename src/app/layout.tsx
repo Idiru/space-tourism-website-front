11d82970507b38ae3f0c1bf9330942d079a3d573
Idir FOUQUET <fouquetidir@gmail.com>
2025-04-01 19:19:28 +0200

    Removed tailwinds / hero done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="!m-0">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased !p-0 !m-0`}
       >
         {children}
       </body>
